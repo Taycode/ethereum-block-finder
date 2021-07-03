ef7d3bf09c92e7c8349047d857f9bead1f46b96b
Abdulmateen Tairu <tay2druh@gmail.com>
2021-07-03 12:14:17 +0100

    setup app

---
+++ app.py
@@ -0,0 +1,13 @@
+"""Main App"""
+
+from flask import Flask
+from flask_cors import CORS
+from controller import api
+
+app = Flask(__name__)
+CORS(app)
+
+api.init_app(app)
+
+if __name__ == "__main__":
+	app.run(debug=True)
---
+++ controller/__init__.py
@@ -0,0 +1,5 @@
+"""Controllers"""
+
+from flask_restx import Api
+
+api = Api(title='ETH Finder App', description='Find ETH Blocks', doc='/docs')
